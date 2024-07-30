681e9a5bee79a12d81d10658a49252a8be042c5b
Brian <java202406@gmail.com>
2024-07-30 21:49:21 +0800

    0730

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -5,13 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Footer from './Footer';
 
+const multiButton=()=>{
+  var output=[];
+  for(let i=0;i<11;++i)
+      output.push(<button>我是第{i}個按鍵</button>)
+  return output;
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-    <Footer />
-  </React.StrictMode>
-);
+root.render(multiButton());
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
